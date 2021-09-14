2a0e3b25d2697797d03888ae9073682e81d4021d
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2021-09-14 15:44:41 -0400

    fix Usage in help. Add code comments

--- dodo-SSG.js
+++ dodo-SSG.js
@@ -4,8 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const argv = require("yargs")
-  .usage("Usage: $0 <command> [options]")
-  .command("input", "Input")
+  .usage("Usage: $0 [options]")
   .alias("i", "input")
   .describe("i", "Load input file or directory")
   .demandOption(["i"])
@@ -19,13 +18,19 @@ let files = [];
 let currentDir = "./";
 let stylesheet = "";
 
-if (fs.statSync(argv.input).isFile()) {
-  files[0] = argv.input;
-}
+if (fs.existsSync(argv.input)) {
+  // if input name is a '.txt' file, save it to files[0]
+  if (fs.statSync(argv.input).isFile() && argv.input.match(/.txt$/)) {
+    files[0] = argv.input;
+  }
 
-if (fs.statSync(argv.input).isDirectory()) {
-  currentDir = "./" + argv.input + "/";
-  files = fs.readdirSync(currentDir).filter((file) => file.match(/.txt$/));
+  // if input name is a directory, save all '.txt' files to files array
+  if (fs.statSync(argv.input).isDirectory()) {
+    currentDir = "./" + argv.input + "/";
+    files = fs.readdirSync(currentDir).filter((file) => file.match(/.txt$/));
+  }
+} else {
+  console.log("file or directory not found!");
 }
 
 if (fs.existsSync("./dist")) {
@@ -52,6 +57,7 @@ files.forEach((file) => {
   // remove first line of body
   body = body.substr(body.indexOf("\n", 1));
 
+  // if user provided a stylesheet, include stylesheet in the html
   if (argv.stylesheet) {
     stylesheet = `<link rel="stylesheet" href="${argv.stylesheet}">`;
   }
@@ -70,7 +76,9 @@ ${body}
 </body>
 </html>`;
 
+  // get filename without its '.txt' extension
   const fileName = path.basename(file, ".txt");
 
+  // write to 'dist' directory new html file
   fs.writeFileSync("./dist/" + fileName + ".html", fullHTML);
 });
