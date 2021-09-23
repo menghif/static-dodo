a350599fe0eaf289534258aa684b62aaf573077a
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2021-09-23 14:06:54 -0400

    Improve import of version number from package.json

--- dodo-SSG.js
+++ dodo-SSG.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs");
 const path = require("path");
-const package = require("./package.json");
+const { version } = require("./package.json");
 
 const argv = require("yargs")
   .usage("Usage: $0 [options]")
@@ -12,7 +12,7 @@ const argv = require("yargs")
   .alias("s", "stylesheet")
   .describe("s", "Provide stylesheet")
   .alias("v", "version")
-  .version(true, `dodo-SSG version: ${package.version}`)
+  .version(true, `dodo-SSG version: ${version}`)
   .alias("h", "help").argv;
 
 let files = [];
