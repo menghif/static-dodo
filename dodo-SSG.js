2eaf3eaa6abf1f1726ed1ca0b366ca9f29666a0d
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2021-10-27 10:37:56 -0400

    Fixes #17. Add full Markdown support

--- dodo-SSG.js
+++ dodo-SSG.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const yargs = require("yargs");
 const { version } = require("./package.json");
+const md = require("markdown-it")();
 
 const argv = yargs
   .usage("Usage: $0 [options]")
@@ -46,30 +47,7 @@ const writeHTMLFile = (title, body, file, fileType, stylesheet) => {
 };
 
 const parseMdFiles = (fullText) => {
-  const fullMdText = fullText
-    .replace(/^###### (.*$)/gim, "<h6>$1</h6>")
-    .replace(/^##### (.*$)/gim, "<h5>$1</h5>")
-    .replace(/^#### (.*$)/gim, "<h4>$1</h4>")
-    .replace(/^### (.*$)/gim, "<h3>$1</h3>")
-    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
-    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
-    .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>")
-    .replace(/\*(.*)\*/gim, "<i>$1</i>")
-    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
-    .replace(/`([^`]*)`/gim, "<code>$1</code>")
-    .replace(/^---$/m, "<hr>")
-    .replace(/^\`\`\`/gim, "")
-    .trim();
-
-  let htmlBody = fullMdText
-    .split(/\r?\n\r?\n/)
-    .map((param) => {
-      if (!param.match(/^<h\d>/gm) && !param.match(/^<hr>$/gm))
-        return `\t<p>${param}</p>\n`;
-      return `\t${param}\n`;
-    })
-    .join(" ");
-
+  let htmlBody = md.render(fullText);
   return htmlBody.substr(htmlBody.indexOf("</h1>", 1) + 6);
 };
 
