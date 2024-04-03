78ef0f81e9185597d8dd19006f39f0455089b01e
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2024-04-03 19:49:24 -0400

    Change argument parser dependecy from yargs to minimist

--- static-dodo.js
+++ static-dodo.js
@@ -2,32 +2,39 @@
 
 const fs = require("fs");
 const path = require("path");
-const yargs = require("yargs");
+const minimist = require("minimist");
 const { version } = require("./package.json");
+const getHelp = require("./lib/help");
 const { parseConfigFile } = require("./lib/config");
 const { parseInput } = require("./lib/input");
 const { processFile } = require("./lib/processFile");
-const argv = yargs
-  .usage("Usage: $0 [options]")
-  .option("i", {
-    alias: "input",
-    describe: "Load input file or directory",
-  })
-  .option("s", {
-    alias: "stylesheet",
-    describe: "Provide stylesheet",
-  })
-  .option("c", {
-    alias: "config",
-    describe: "Provide a path to a JSON file",
-  })
-  .alias("v", "version")
-  .version(true, `static-dodo version: ${version}`)
-  .alias("h", "help").argv;
+
+const argv = minimist(process.argv.slice(2), {
+  string: ["input", "stylesheet", "config"],
+  boolean: ["version", "help"],
+  alias: { i: "input", s: "stylesheet", c: "config", v: "version", h: "help" },
+  unknown: (unknownArgument) => {
+    console.error(`Option '${unknownArgument}' not found.`);
+  },
+});
+
+if (argv.version) {
+  console.log(version);
+  process.exit(0);
+}
+
+if (argv.help) {
+  getHelp();
+  process.exit(0);
+}
+
+if (argv.input) {
+  console.log(argv.input);
+}
 
 if (!argv.input && !argv.config) {
-  yargs.showHelp();
-  process.exit(1);
+  getHelp();
+  process.exit(0);
 }
 
 if (fs.existsSync(path.join(process.cwd(), "dist"))) {
